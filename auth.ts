d4405b9dda781a9f632bd1a696b7a851448ea813
Morgane Mottey <morganemottey.perso@gmail.com>
2026-07-07 21:51:42 +0200

    fix: remove prisma adapter from auth

--- auth.ts
+++ auth.ts
@@ -1,11 +1,8 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
